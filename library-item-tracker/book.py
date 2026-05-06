696b6b2d87efdbe32dc37d957e2ad792514a11a9
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-06 11:32:56 -0400

    Added class attribute, class method, and static method

--- library-item-tracker/book.py
+++ library-item-tracker/book.py
@@ -1,5 +1,17 @@
 class Book:
+    library_name = "Central Library"
+
     def __init__(self, title, author, available=True):
         self.title = title
         self.author = author
         self.available = available
+
+    @classmethod
+    def change_library_name(cls, new_name):
+        cls.library_name = new_name
+
+    @staticmethod
+    def is_valid_title(title):
+        return len(title.strip()) > 0
+    
+    
--- library-item-tracker/main.py
+++ library-item-tracker/main.py
@@ -5,3 +5,16 @@ book2 = Book("AI Intro", "Jane Smith", False)
 
 print(book1.title, book1.author, book1.available)
 print(book2.title, book2.author, book2.available)
+
+# Class + Static methods testing 
+print("\nClass and Static Methods:")
+
+print(Book.library_name)
+
+Book.change_library_name("City Library")
+print(Book.library_name)
+
+print(Book.is_valid_title("Python"))
+print(Book.is_valid_title(""))
+
+
