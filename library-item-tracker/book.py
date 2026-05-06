c13664c8fba983ebeb8293c163539a5f9a456879
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-06 10:45:11 -0400

    Added intance methods and display_info of book1

--- library-item-tracker/book.py
+++ library-item-tracker/book.py
@@ -3,3 +3,18 @@ class Book:
         self.title = title
         self.author = author
         self.available = available
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"{self.title}  bookborrowed.")
+        else:
+            print(f"{self.title} book is not available.")
+
+    def return_book(self):
+        self.available = True
+        print(f"{self.title} book returned.")
+
+    def display_info(self):
+        status = "Available" if self.available else "book Not Available"
+        print(f"Title: {self.title}, Author: {self.author}, Status: {status}")      
--- library-item-tracker/main.py
+++ library-item-tracker/main.py
@@ -1,7 +1,16 @@
 from book import Book
 
 book1 = Book("Python Basics", "John Doe")
-book2 = Book("AI Intro", "Jane Smith", False)
+book2 = Book("Urdu Poetry", "Iqbal Lahori", False)
 
 print(book1.title, book1.author, book1.available)
 print(book2.title, book2.author, book2.available)
+
+print("\nTesting Methods for book1 and book2")
+
+# Test methods on book1
+book1.display_info()
+book1.borrow()
+book1.display_info()
+book1.return_book()
+book1.display_info()
