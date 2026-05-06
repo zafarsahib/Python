751d6e7d716354f7cbb476c30f4017cd6fe0991d
Muhammad Zafar <85223983+zafarsahib@users.noreply.github.com>
2026-05-06 10:21:15 -0400

    Initialized book and main classes attributes

---
+++ library-item-tracker/book.py
@@ -0,0 +1,5 @@
+class Book:
+    def __init__(self, title, author, available=True):
+        self.title = title
+        self.author = author
+        self.available = available
---
+++ library-item-tracker/main.py
@@ -0,0 +1,7 @@
+from book import Book
+
+book1 = Book("Python Basics", "John Doe")
+book2 = Book("AI Intro", "Jane Smith", False)
+
+print(book1.title, book1.author, book1.available)
+print(book2.title, book2.author, book2.available)
